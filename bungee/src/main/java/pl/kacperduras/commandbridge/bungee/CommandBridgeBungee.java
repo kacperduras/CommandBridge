02ef3adaf070a29b0cc7e27c59ff3710ed4e1606
kacperduras <git@kacperduras.pl>
2018-04-07 20:55:24 +0200

    Add receiving messages on BungeeCord Bridge

--- bungee/src/main/java/pl/kacperduras/commandbridge/bungee/CommandBridgeBungee.java
+++ bungee/src/main/java/pl/kacperduras/commandbridge/bungee/CommandBridgeBungee.java
@@ -1,19 +1,24 @@
 package pl.kacperduras.commandbridge.bungee;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.event.PluginMessageEvent;
+import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.event.EventHandler;
 import org.apache.commons.lang3.Validate;
 import pl.kacperduras.commandbridge.shaded.CommandBridgeAPI;
 import pl.kacperduras.commandbridge.shaded.CommandBridgeConfig;
 import pl.kacperduras.commandbridge.shaded.util.ConfigUtils;
 
-public final class CommandBridgeBungee extends Plugin implements CommandBridgeAPI {
+public final class CommandBridgeBungee extends Plugin implements CommandBridgeAPI, Listener {
 
   private CommandBridgeConfig config;
 
@@ -32,6 +37,8 @@ public final class CommandBridgeBungee extends Plugin implements CommandBridgeAP
     if (this.config.getChannels().getBungeeCord().isEnabled()) {
       this.getProxy().registerChannel(this.config.getChannels().getBungeeCord().getName());
     }
+
+    this.getProxy().getPluginManager().registerListener(this, this);
   }
 
   @Override
@@ -84,6 +91,29 @@ public final class CommandBridgeBungee extends Plugin implements CommandBridgeAP
   @Override
   public boolean isBungee() {
     return true;
+  }
+
+  @EventHandler
+  public void onReceive(PluginMessageEvent event) {
+    String channel = event.getTag();
+    if (!channel.equals(this.config.getChannels().getBungeeCord().getName())) {
+      return;
+    }
+
+    DataInputStream input = new DataInputStream(new ByteArrayInputStream(event.getData()));
+    try {
+      String nickname = input.readUTF();
+      String command = input.readUTF();
+
+      ProxiedPlayer target = this.getProxy().getPlayer(nickname);
+      if (target == null) {
+        return;
+      }
+
+      this.getProxy().getPluginManager().dispatchCommand(target, command);
+    } catch (IOException ex) {
+      ex.printStackTrace();
+    }
   }
 
 }
